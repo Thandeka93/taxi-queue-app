3e1ac8236e1683fe1fa8e75b69eed6c0b0dde9ac
Thandeka93 <noluthandombali4@gmail.com>
2023-05-25 14:33:16 +0200

    taxi

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -1,7 +1,41 @@
 // write your DOM code here.
 
-
 // DOM element references
+const passenger_queue_count= document.querySelector(".passenger_queue_count");
+const joinBtn = document.querySelector(".joinBtn");
+const leaveBtn = document.querySelector(".leaveBtn");
+const taxi_queue_count = document.querySelector(".taxi_queue_count");
+const taxiJoinBtn = document.querySelector(".taxiJoinBtn");
+const departBtn = document.querySelector(".departBtn");
+const countElement = document.querySelector(".passenger_queue_count")
+
+let passengers = 0;
+
+function countPassenger() {
+  if (passengers > 0) {
+    passengers--;
+  } else {
+    passengers++;
+  }
+  
+  countElement.innerHTML = passengers;
+}
+
+joinBtn.addEventListener('click', countPassenger);
+leaveBtn.addEventListener('click', countPassenger);
+
+function manageTaxiQueue(move, queue) {
+    if (move === 'join') {
+      queue++;
+    } else if (move === 'leave' && queue >= 12) {
+      queue--;
+    }
+    return queue;
+  }
+  
+  
+
+
 
 // create Factory Function instance
 
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,28 +1,37 @@
 function TaxiQueue() {
-
+	let queue = [];
+	let taxiQueue = [];
 
 	function joinQueue() {
-
+		queue.push(1);
 	}
 
 	function leaveQueue() {
-
+	 queue.pop();
 	}
 
 	function joinTaxiQueue() {
-
+		taxiQueue.push(1);
 	}
 
 	function queueLength() {
 
+	 return queue.length;
+
+
 	}
 
 	function taxiQueueLength() {
-
+		return taxiQueue.length;
 	}
 
-	function taxiDepart(){
-
+	function taxiDepart() {
+		if (taxiQueue.length > 0 && queue.length >= 4) {
+			for (let i = 0; i < 4; i++) {
+			  queue.shift();
+			}
+			taxiQueue.pop();
+		  }
 	}
 
 	return {
